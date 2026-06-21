37ac7e5a0c6d1423c49aa906ce06ab999c57763b
Ali MHD <alimohammadi.mhd.1377@gmail.com>
2026-06-21 10:08:42 -0700

    اصلاح بعد از دیدن کد استاد

--- Number Guesser Game/main.py
+++ Number Guesser Game/main.py
@@ -7,35 +7,45 @@
 import random
 
 
-score = 100
-random_number = random.randint(1, 100)
+def statr_game():
+    score = 100
+    random_number = random.randint(1, 100)
 
-game_over = False
-while not game_over:
-    while True:
-        try:
-            n = input("guess a number: ")
-            if n == 'q':
-                game_over = True
+    game_over = False
+    while not game_over:
+        while True:
+            try:
+                user_guess = input("Enter your guess between 1 and 100: ")
+                if user_guess == 'q':
+                    game_over = True
+                    break
+                user_guess = int(user_guess)
                 break
-            n = int(n)
+            except:
+                print("لطفا ورودی صحیحی را وارد کنید")
+
+        if game_over == True:
+            print("خداحافظ")
             break
-        except:
-            print("guess a valid number please: ")
 
-    if game_over == True:
-        print("خداحافظ")
-        break
-
-    if n == random_number:
-        print("حدس شما درست بود")
-        print("score:", score)
-        break
-
-    elif n > random_number:
-        print("عدد کمتری را حدس بزنید")
+        if user_guess == random_number:
+            print("حدس شما درست بود")
+            print("your score:", score)
+            wanna_play = input("می خوای دوباره بازی کنی؟ y/n: ")
+            if wanna_play == 'y':
+                random_number = random.randint(1, 100)
+                score = 100
+                continue
+            else:
+                print("خداحافظ")
+                break
+        elif user_guess > random_number:
+            print("عدد کمتری را حدس بزنید")
+            
+        else:
+            print("عدد بزرگتری را حدس بزنید")
+        
         score -= 1
 
-    else:
-        print("عدد بزرگتری را حدس بزنید")
-        score -= 1
+if __name__ == '__main__':
+    statr_game()
